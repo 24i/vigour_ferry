81e4a1042e927003e17390c1a681c1f4f00c1b9e
Shawn Inder <shawn@vigour.io>
2015-08-14 16:36:19 +0200

    Add tests for headers

--- test/node/server/index.js
+++ test/node/server/index.js
@@ -1,5 +1,8 @@
 var http = require('http')
 var log = require('npmlog')
+var Promise = require('promise')
+var fs = require('vigour-fs')
+var readJSON = Promise.denodeify(fs.readJSON)
 var packer = require('../../../')
 var startupTimeout = 10000
 var port = 8000
@@ -19,6 +22,7 @@ var options =
 					, token: slackToken
 					, channel: "directv-packers" }
 				, port: port
+				, akamai: true
 				}
 			}
 		}
@@ -66,6 +70,223 @@ describe("Server", function () {
 			expect(err).not.to.exist
 		})
 		req.end()
+	})
+
+	describe("Response headers", function () {
+		var base = "http://localhost:" + port
+		var maxage = 31556900	// ~ 1 year in seconds
+		function request (url) {
+			return new Promise(function (resolve, reject) {
+				var req = http.request(url, function (res) {
+					res.on('error', reject)
+					// console.log("headers", res.headers)
+					resolve(res)
+				})
+				req.on('error', reject)
+				req.end()
+			})
+		}
+
+		
+		describe("/", function () {
+			var url = base + "/"
+			var reqPromise
+			before(function(){
+				reqPromise = request(url)
+			})
+			
+			it("should include `access-control-allow-origin=*"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['access-control-allow-origin'])
+						.to.equal("*")
+				})
+				.done(done)
+			})
+
+			it("should include `cache-control=public, max-age=0"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['cache-control'])
+						.to.equal("public, max-age=0")
+				})
+				.done(done)
+			})
+
+			it("should include `edge-control=public, max-age=0"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['edge-control'])
+						.to.equal("public, max-age=0")
+				})
+				.done(done)
+			})
+		})
+
+		describe("/manifest.json", function () {
+			var url = base + "/manifest.json"
+			var reqPromise
+			before(function(){
+				reqPromise = request(url)
+			})
+
+			it("should include `access-control-allow-origin=*"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['access-control-allow-origin'])
+						.to.equal("*")
+				})
+				.done(done)
+			})
+			it("should include `cache-control=public, max-age=0"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['cache-control'])
+						.to.equal('public, max-age=0')
+				})
+				.done(done)
+			})
+			it("should include `edge-control=public, max-age=0"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['edge-control'])
+						.to.equal('public, max-age=0')
+				})
+				.done(done)
+			})
+		})
+
+		describe("/favicon.ico", function () {
+			var url = base + "/favicon.ico"
+			var reqPromise
+			before(function(){
+				reqPromise = request(url)
+			})
+
+			it("should include `access-control-allow-origin=*"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['access-control-allow-origin'])
+						.to.equal("*")
+				})
+				.done(done)
+			})
+			it("should include `cache-control=public, no-transform, max-age="
+				+ maxage
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['cache-control'])
+						.to.equal("public, no-transform, max-age=" + maxage)
+				})
+				.done(done)
+			})
+			it("should include `edge-control=public, max-age=0"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['edge-control'])
+						.to.equal('public, max-age=0')
+				})
+					.done(done)
+			})
+		})
+
+		describe("/geo", function () {
+			var url = base + "/geo"
+			var reqPromise
+			before(function(){
+				reqPromise = request(url)
+			})
+
+			it("should include `cache-control=public, max-age=0"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['cache-control'])
+						.to.equal('public, max-age=0')
+				})
+				.done(done)
+			})
+			it("should include `edge-control=!no-cache, max-age=" + maxage
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['edge-control'])
+						.to.equal("!no-cache, max-age=" + maxage)
+				})
+					.done(done)
+			})
+		})
+
+		describe("/native/:sha/package.json", function () {
+			var reqPromise
+			before(function (done) {
+				readJSON(base + "/manifest.json")
+					.then(function (manifest) {
+						var latestSHA = manifest.commitHash
+						var url = base + "/native/" + latestSHA + "/package.json"
+						reqPromise = request(url)
+					})
+					.done(done)
+			})
+
+			it("should include `access-control-allow-origin=*"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['access-control-allow-origin'])
+						.to.equal("*")
+				})
+				.done(done)
+			})
+			it("should include `cache-control=public, no-transform, max-age="
+				+ maxage
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['cache-control'])
+						.to.equal('public, no-transform, max-age=' + maxage)
+				})
+				.done(done)
+			})
+			it("should include `edge-control=!no-cache, max-age="
+				+ maxage
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['edge-control'])
+						.to.equal('!no-cache, max-age=' + maxage)
+				})
+				.done(done)
+			})
+		})
+
+		describe("/inexistent", function () {
+			var url = base + "/inexistent"
+			var reqPromise
+			before(function(){
+				reqPromise = request(url)
+			})
+
+			it("should include `access-control-allow-origin=*"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['access-control-allow-origin'])
+						.to.equal('*')
+				})
+				.done(done)
+			})
+			it("should include `cache-control=public, max-age=0"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['cache-control'])
+						.to.equal('public, max-age=0')
+				})
+				.done(done)
+			})
+			it("should include `edge-control=public, max-age=0"
+			, function (done) {
+				reqPromise.then(function (res) {
+					expect(res.headers['edge-control'])
+						.to.equal('public, max-age=0')
+				})
+				.done(done)
+			})
+		})
 	})
 
 	it("should respond to `POST /status`", function (done) {
